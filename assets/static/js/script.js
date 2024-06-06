fd86ef23a2dffb0b8e0d392a79278d39cee1aa15
valeriarojas616 <valeria.rojas.616@gmail.com>
2024-06-06 15:32:51 -0600

    06-06-24
    Primer modelo de alerta para borrar elementos de mis productos.

--- assets/static/js/script.js
+++ assets/static/js/script.js
@@ -1,4 +1,24 @@
-//capturar boton trash
-const trashButton = document.querySelectorAll('.trash-button');
-console.log(trashButton);
-// icono
+document.addEventListener('DOMContentLoaded', ()=>{
+    //capturar boton
+    const trashButton = document.querySelectorAll('.trash-button');
+
+    // Iterar sobre cada botón y añadir un event listener para el clic
+    trashButton.forEach(button => {
+        button.addEventListener('click', () => {
+            // Mostrar una confirmación al usuario
+            const userConfirmed = confirm('¿Deseas eliminar este producto?');
+
+            // Si el usuario confirma, eliminar el contenedor padre
+            if (userConfirmed) {
+                // Capturar el contenedor ficha más cercano
+                const containerProductFicha = button.closest('.container-product-ficha');
+
+                // Eliminar el contenedor padre
+                if (containerProductFicha) {
+                    containerProductFicha.remove();
+                }
+            }
+        });
+    });
+});
+
