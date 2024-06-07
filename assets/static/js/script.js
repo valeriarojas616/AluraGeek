3c97d0a9b6350edc9bec815a267cd7558518d8fd
valeriarojas616 <valeria.rojas.616@gmail.com>
2024-06-07 13:35:34 -0600

    07-06-24 Mejora  y personalizacion en la ventana emergente para eliminar productos a traves de javascript.

--- assets/static/js/script.js
+++ assets/static/js/script.js
@@ -1,24 +1,44 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     //capturar boton
-    const trashButton = document.querySelectorAll('.trash-button');
+    const trashButtons = document.querySelectorAll('.trash-button');
 
-    // Iterar sobre cada botón y añadir un event listener para el clic
-    trashButton.forEach(button => {
-        button.addEventListener('click', () => {
-            // Mostrar una confirmación al usuario
-            const userConfirmed = confirm('¿Deseas eliminar este producto?');
+    function buttonTrashFunction(){
+        // Iterar sobre cada botón y añadir un event listener para el clic
+        trashButtons.forEach(button => {
+            button.addEventListener('click', () => {
 
-            // Si el usuario confirma, eliminar el contenedor padre
-            if (userConfirmed) {
-                // Capturar el contenedor ficha más cercano
-                const containerProductFicha = button.closest('.container-product-ficha');
+                //tomar el elemento donde se añadira nuevo contenido
+                const documentoPrincipal = document.querySelector('main');
 
-                // Eliminar el contenedor padre
-                if (containerProductFicha) {
-                    containerProductFicha.remove();
-                }
-            }
+                //declarar el elemento que se añade
+                const ventanaEmergente = `<div class="alert-window" id="confirm-trash">
+                <div class="alert-window-content">
+                    <p class="alert-window-title">¿Deseas eliminar este producto?</p>
+                    <div class="section-buttons-alert">
+                        <button class="alert-window-button" id="confirm-yes">Sí</button>
+                        <button class="alert-window-button" id="confirm-no">No</button>
+                    </div>
+                </div>
+            </div>`
+            //introducir elemento
+            documentoPrincipal.insertAdjacentHTML('beforeend', ventanaEmergente);
+
+            // Añadir event listeners a los botones dentro de la ventana emergente
+            document.getElementById('confirm-yes').addEventListener('click', () => {
+                // Acción para eliminar el producto
+                document.querySelector('.container-product-ficha').remove();
+                //cerrar ventana emergente
+                document.getElementById('confirm-trash').remove();
+            });
+
+            document.getElementById('confirm-no').addEventListener('click', () => {
+                // Cerrar la ventana emergente sin eliminar
+                document.getElementById('confirm-trash').remove();
+            });
+            
+            });
         });
-    });
+    };
+    buttonTrashFunction();
 });
 
